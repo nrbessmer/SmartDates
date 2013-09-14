99971735b9fe8767757ac661bd1c73e0db4207af
Nicholas Bessmer <nrbessmer@gmail.com>
2013-09-13 23:19:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartDates::Application.config.secret_key_base = '27bddf70da847c1140bbbc370c44cc1076fee66355e2aa6ae7f5b5b9a2ace9d73fbb19ce492f2d22e046b68055d9fa91dc7de53265667344587aec9d677d6ea5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartDates::Application.config.session_store :cookie_store, key: '_SmartDates_session'
